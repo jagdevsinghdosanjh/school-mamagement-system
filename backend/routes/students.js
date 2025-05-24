1eb5f037fdb755521c9ef6c3bbf4207d01c6f623
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-05-24 19:56:47 +0530

    Ok

--- backend/routes/students.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/", async (req, res) => {
-    // Fetch students from database
-    res.json([{ id: 1, name: "John Doe", email: "john@example.com" }]);
-});
-
-export default router;
